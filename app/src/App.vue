1ec02ccddcafe97470a34bd86710892763a437d3
Matheus Oliveira <oliveira.matheus@cwi.com.br>
2020-10-19 15:13:17 -0300

    criando app e adicionando roteamento

---
+++ app/src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <v-navigation-drawer app permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Modulo Academico
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              <router-link to="/">{{ item.title }}</router-link>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content>
+        <v-container fluid>
+            <v-row class="fill-height">
+                <v-col>
+                    <transition name="fade">
+                        <router-view></router-view>
+                    </transition>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-content>
+  </div>
+</template>
+
+ <!-- <router-link to="/">Home</router-link> | -->
+      <!-- <router-link to="/about">About</router-link> -->
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { title: 'Alunos', icon: 'mdi-view-dashboard' },
+        ],
+        right: null,
+      }
+    },
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
